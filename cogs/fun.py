b7a603e2a768b1f350423d7678c0d495fb28edcd
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-07-06 03:24:18 +0000

    modified hybrid commands

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,16 +1,17 @@
 import discord
 from discord.ext import commands
 import random
-from discord import Member
-from asciify import asciify
+from discord import Member, File
 from io import BytesIO
 from PIL import Image
 from main import Bot
-import requests
 from typing import Union
 from emojify import emojify_image
 from jokeapi import Jokes
 import aiohttp
+import requests
+from asciify import asciify
+
 
 class Fun(commands.Cog):
 
@@ -21,7 +22,7 @@ class Fun(commands.Cog):
     async def close(self):
         await self.session.close()
 
-    @commands.hybrid_command()
+    @commands.command()
     async def wanted(self, ctx: commands.Context, *, member: discord.Member):
         await ctx.defer()
         wanted = Image.open("wanted.jpg")
@@ -42,7 +43,7 @@ class Fun(commands.Cog):
                 number = 3 * number + 1
         return number == 1
 
-    @commands.hybrid_command()
+    @commands.command()
     async def collatz(self, ctx, number: int):
         await ctx.defer()
         original_number = int(number)
@@ -61,7 +62,7 @@ class Fun(commands.Cog):
         else:
             await ctx.send(f"The number {number} is not a Collatz conjecture.")
 
-    @commands.hybrid_command(aliases=['peepee', 'pipi', 'pepe'])
+    @commands.command(aliases=['peepee', 'pipi', 'pepe'])
     async def pp(self, ctx):
         await ctx.defer()
         user = ctx.author.name
@@ -78,11 +79,12 @@ class Fun(commands.Cog):
         embed.set_footer(text=f"Requested By: {user}", icon_url=footer_pfp)
         await ctx.send(embed=embed)
 
-    @commands.hybrid_command()
+    @commands.command()
     async def emojify(self, ctx, url: Union[discord.Member, str], size: int):
         await ctx.defer()
         if isinstance(url, discord.Member):
             url = url.display_avatar.url
+
         def get_emojified_image():
             r = requests.get(url, stream=True)
             image = Image.open(r.raw).convert("RGB")
@@ -90,15 +92,19 @@ class Fun(commands.Cog):
             if size > size:
                 res = f"```{res}```"
             return res
+
         result = await self.bot.loop.run_in_executor(None, get_emojified_image)
         await ctx.send(f"```py\n{result}```")
 
-    @commands.hybrid_command()
-    async def asciify(self, ctx: commands.Context, link: Union[Member, str], new_width: int = 100):
+    @commands.command()
+    async def asciify(self,
+                      ctx: commands.Context,
+                      link: Union[Member, str],
+                      new_width: int = 100):
         await ctx.defer()
         await asciify(ctx, link, new_width)
 
-    @commands.hybrid_command()
+    @commands.command()
     async def rng(self, ctx):
         await ctx.defer()
         rand_int = random.randint(0, 1000)
@@ -109,7 +115,7 @@ class Fun(commands.Cog):
         embed.add_field(name="Number:", value=f"```py\n{rand_int}```")
         await ctx.send(embed=embed)
 
-    @commands.hybrid_command()
+    @commands.command()
     async def joke(self, ctx: commands.Context):
         await ctx.defer()
         j = await Jokes()
@@ -124,6 +130,7 @@ class Fun(commands.Cog):
             msg = joke["setup"]
             msg += f"||{joke['delivery']}||"
         await ctx.send(msg)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Fun(bot))
