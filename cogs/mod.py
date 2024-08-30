a25f7421723ad29d6423322fad7cffa4e16503b2
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-08-30 11:16:34 +0000

    added change nickname command

--- cogs/mod.py
+++ cogs/mod.py
@@ -5,7 +5,7 @@ import aiohttp
 import asyncio
 import io
 import time
-
+from discord import app_commands
 
 # Utility Functions
 async def _get_channel_properties(channel: discord.TextChannel) -> dict:
@@ -491,6 +491,45 @@ class Moderation(commands.Cog):
                 await ctx.send(f"Nuked and recreated {new_channel.mention}.")
         except discord.HTTPException:
             pass
+
+    @commands.hybrid_command(
+        name="nick",
+        description="Change the nickname of a user on a server.",
+    )
+    @commands.has_permissions(manage_nicknames=True)
+    @commands.bot_has_permissions(manage_nicknames=True)
+    @app_commands.describe(
+        user="The user that should have a new nickname.",
+        nickname="The new nickname that should be set.",
+    )
+    async def nick(
+        self, context: Context, user: discord.User, *, nickname: str = None
+    ) -> None:
+        """
+        Change the nickname of a user on a server.
+
+        :param context: The hybrid command context.
+        :param user: The user that should have its nickname changed.
+        :param nickname: The new nickname of the user. Default is None, which will reset the nickname.
+        """
+        member = context.guild.get_member(user.id) or await context.guild.fetch_member(
+            user.id
+        )
+        try:
+            await member.edit(nick=nickname)
+            embed = discord.Embed(
+                description=f"**{member}'s** new nickname is **{nickname}**!",
+                color=0xBEBEFE,
+            )
+            await context.send(embed=embed)
+        except:
+            embed = discord.Embed(
+                description="An error occurred while trying to change the nickname of the user. Make sure my role is above the role of the user you want to change the nickname.",
+                color=0xE02B2B,
+            )
+            await context.send(embed=embed)
+
+    
 
     @commands.hybrid_command()
     async def ping(self, ctx: commands.Context):
