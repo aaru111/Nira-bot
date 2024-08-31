042f7b12744b0fa0688166646cf90812b86daa5d
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-08-31 12:11:58 +0000

    fixed imports

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,3 +1,5 @@
 from pkgutil import iter_modules
 
-EXTENSIONS = [module.name for module in iter_modules(__path__, f'{__package__}.')]
+EXTENSIONS = [
+    module.name for module in iter_modules(__path__, f'{__package__}.')
+]
