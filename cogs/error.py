c3d6570a8d1cb68a3295e115f5b8e337901ee38c
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-09-03 02:19:38 +0000

    Refactored error handling with type hints, improved rate limit handling, and added command suggestion for unknown commands.

--- cogs/error.py
+++ cogs/error.py
@@ -2,11 +2,13 @@ import discord
 from discord.ext import commands
 import traceback
 import aiohttp
+import asyncio
 from difflib import get_close_matches
+from typing import Optional, Tuple, Any, List
 
 # Define the embed color
-EMBED_COLOR = 0x2f3131
-DELETE_AFTER = 10  # Time in seconds after which the error message will delete itself
+EMBED_COLOR: int = 0x2f3131
+DELETE_AFTER: int = 10  # Time in seconds after which the error message will delete itself
 
 
 class Errors(commands.Cog):
@@ -21,10 +23,10 @@ class Errors(commands.Cog):
         Args:
             bot (commands.Bot): The instance of the bot.
         """
-        self.bot = bot
+        self.bot: commands.Bot = bot
         self.session: aiohttp.ClientSession = aiohttp.ClientSession()
 
-    async def cog_unload(self):
+    async def cog_unload(self) -> None:
         """Clean up resources when the cog is unloaded."""
         await self.session.close()
 
@@ -40,14 +42,17 @@ class Errors(commands.Cog):
             description (str): The description of the embed.
             button_color (discord.ButtonStyle): The color of the help button.
         """
-        embed = discord.Embed(title=title,
-                              description=f"```py\n{description}```",
-                              color=EMBED_COLOR)
-        view = discord.ui.View()
+        embed: discord.Embed = discord.Embed(
+            title=title,
+            description=f"```py\n{description}```",
+            color=EMBED_COLOR)
+        view: discord.ui.View = discord.ui.View()
         view.add_item(HelpButton(ctx, title, description, button_color))
 
         try:
-            message = await ctx.send(embed=embed, view=view, ephemeral=True)
+            message: discord.Message = await ctx.send(embed=embed,
+                                                      view=view,
+                                                      ephemeral=True)
             await message.delete(
                 delay=DELETE_AFTER
             )  # Attempt to delete the message after a delay
@@ -74,7 +79,7 @@ class Errors(commands.Cog):
             description (str): The description of the error.
             title (str): The title of the error.
         """
-        button_color = self.get_button_color(title)
+        button_color: discord.ButtonStyle = self.get_button_color(title)
         await self.send_error_embed(ctx, title, description, button_color)
 
     def get_button_color(self, title: str) -> discord.ButtonStyle:
@@ -87,31 +92,32 @@ class Errors(commands.Cog):
         Returns:
             discord.ButtonStyle: The color of the button.
         """
-        minor_errors = [
+        minor_errors: List[str] = [
             "Missing Required Argument", "Command Not Found",
             "User Input Error", "Invalid End of Quoted String",
             "Expected Closing Quote", "Unexpected Quote", "Bad Argument"
         ]
 
-        major_errors = [
+        major_errors: List[str] = [
             "Missing Permissions", "Bot Missing Permissions",
             "Command on Cooldown", "No Private Message", "Check Failure",
             "Disabled Command", "NSFW Channel Required"
         ]
 
-        critical_errors = [
+        critical_errors: List[str] = [
             "Not Owner", "Forbidden", "Not Found", "HTTP Exception",
             "Max Concurrency Reached", "Unexpected Error"
         ]
 
-        if title in minor_errors:
-            return discord.ButtonStyle.success  # Green
-        elif title in major_errors:
-            return discord.ButtonStyle.primary  # Blue
-        elif title in critical_errors:
-            return discord.ButtonStyle.danger  # Red
-        else:
-            return discord.ButtonStyle.secondary  # Grey
+        match title:
+            case title if title in minor_errors:
+                return discord.ButtonStyle.success  # Green
+            case title if title in major_errors:
+                return discord.ButtonStyle.primary  # Blue
+            case title if title in critical_errors:
+                return discord.ButtonStyle.danger  # Red
+            case _:
+                return discord.ButtonStyle.secondary  # Grey
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context,
@@ -123,24 +129,38 @@ class Errors(commands.Cog):
             ctx (commands.Context): The context in which the command was invoked.
             error (commands.CommandError): The error that was raised.
         """
+        # Handling rate limit errors (HTTP 429)
         if isinstance(error,
                       discord.errors.HTTPException) and error.status == 429:
-            # Log the rate limit issue
-            print("Hit rate limit. Retrying after delay.")
-            return  # You can also add a retry mechanism here if necessary
+            retry_after: int = int(error.response.headers.get(
+                "Retry-After", 5))
+            await ctx.send(
+                f"Rate limit hit! Retrying after {retry_after} seconds...",
+                delete_after=DELETE_AFTER)
+            await asyncio.sleep(retry_after)
 
-        command_name = ctx.command.qualified_name if ctx.command else "Unknown Command"
-        command_signature = getattr(ctx.command, 'signature',
-                                    '') if ctx.command else ""
+            # Retry the command
+            try:
+                await ctx.reinvoke()
+            except discord.errors.HTTPException as e:
+                if e.status == 429:
+                    print("Hit rate limit again. Aborting retry.")
+                else:
+                    await self.on_command_error(ctx, e)
+            return
+
+        # Handle other errors
+        command_name: str = ctx.command.qualified_name if ctx.command else "Unknown Command"
+        command_signature: str = getattr(ctx.command, 'signature',
+                                         '') if ctx.command else ""
 
         title, description = self.get_error_title_and_description(
             ctx, error, command_name, command_signature)
         await self.handle_error(ctx, error, description, title)
 
-    def get_error_title_and_description(self, ctx: commands.Context,
-                                        error: commands.CommandError,
-                                        command_name: str,
-                                        command_signature: str) -> tuple[str, str]:
+    def get_error_title_and_description(
+            self, ctx: commands.Context, error: commands.CommandError,
+            command_name: str, command_signature: str) -> Tuple[str, str]:
         """
         Get the title and description for the error embed based on the error type.
 
@@ -153,116 +173,108 @@ class Errors(commands.Cog):
         Returns:
             tuple: The title and description for the error embed.
         """
-        if isinstance(error, commands.MissingRequiredArgument):
-            return (
-                "Missing Required Argument",
-                f"Argument: '{error.param.name}'\nUsage: {ctx.prefix}{command_name} {command_signature}"
-            )
+        match error:
+            case commands.MissingRequiredArgument():
+                return (
+                    "Missing Required Argument",
+                    f"Argument: '{error.param.name}'\nUsage: {ctx.prefix}{command_name} {command_signature}"
+                )
+            case commands.CommandNotFound():
+                # Get the attempted command name
+                attempted_command = ctx.message.content.split(
+                )[0][len(ctx.prefix):]
 
-        elif isinstance(error, commands.CommandNotFound):
-            similar_commands = get_close_matches(
-                error.args[0].split()[0],
-                [cmd.name for cmd in self.bot.commands],
-                n=3,
-                cutoff=0.6)
-            description = "Command not found. Please check your command and try again."
-            if similar_commands:
-                description += "\n\nDid you mean?\n" + '\n'.join(
-                    similar_commands)
-            return ("Command Not Found", description)
+                # Find similar commands
+                similar_commands: List[str] = get_close_matches(
+                    attempted_command, [cmd.name for cmd in self.bot.commands],
+                    n=1,
+                    cutoff=0.6)
 
-        elif isinstance(error, commands.MissingPermissions):
-            perms = ', '.join(error.missing_permissions)
-            return (
-                "Missing Permissions",
-                f"You need the following permissions to execute this command: {perms}"
-            )
+                if similar_commands:
+                    suggestion: str = similar_commands[0]
+                    description: str = f"Command not found. Did you mean `{ctx.prefix}{suggestion}`?"
+                else:
+                    description = "Command not found. Please check your command and try again."
 
-        elif isinstance(error, commands.BotMissingPermissions):
-            perms = ', '.join(error.missing_permissions)
-            return (
-                "Bot Missing Permissions",
-                f"I need the following permissions to execute this command: {perms}"
-            )
-
-        elif isinstance(error, commands.CommandOnCooldown):
-            return (
-                "Command on Cooldown",
-                f"This command is on cooldown. Try again after {error.retry_after:.2f} seconds."
-            )
-
-        elif isinstance(error, commands.NotOwner):
-            return ("Not Owner", "Only the bot owner can use this command.")
-
-        elif isinstance(error, commands.NoPrivateMessage):
-            return (
-                "No Private Message",
-                "This command cannot be used in private messages. Please use it in a server channel."
-            )
-
-        elif isinstance(error, commands.BadArgument):
-            return ("Bad Argument", str(error))
-
-        elif isinstance(error, commands.CheckFailure):
-            return ("Check Failure",
-                    "You do not have permission to execute this command.")
-
-        elif isinstance(error, commands.DisabledCommand):
-            return ("Disabled Command", "This command is currently disabled.")
-
-        elif isinstance(error, commands.UserInputError):
-            return ("User Input Error", str(error))
-
-        elif isinstance(error, commands.InvalidEndOfQuotedStringError):
-            return ("Invalid End of Quoted String", str(error))
-
-        elif isinstance(error, commands.ExpectedClosingQuoteError):
-            return ("Expected Closing Quote", str(error))
-
-        elif isinstance(error, commands.MaxConcurrencyReached):
-            return (
-                "Max Concurrency Reached",
-                f"This command can only be used {error.number} times concurrently."
-            )
-
-        elif isinstance(error, commands.UnexpectedQuoteError):
-            return ("Unexpected Quote", str(error))
-
-        elif isinstance(error, discord.Forbidden):
-            return ("Forbidden", "I do not have permission to do that.")
-
-        elif isinstance(error, discord.NotFound):
-            return ("Not Found", "The requested resource was not found.")
-
-        elif isinstance(error, discord.HTTPException):
-            return ("HTTP Exception", "An HTTP exception occurred.")
-
-        else:
-            # Log the error with traceback
-            traceback_str = ''.join(
-                traceback.format_exception(type(error), error,
-                                           error.__traceback__))
-            return ("Unexpected Error", traceback_str)
+                return ("Command Not Found", description)
+            case commands.MissingPermissions():
+                perms: str = ', '.join(error.missing_permissions)
+                return (
+                    "Missing Permissions",
+                    f"You need the following permissions to execute this command: {perms}"
+                )
+            case commands.BotMissingPermissions():
+                perms: str = ', '.join(error.missing_permissions)
+                return (
+                    "Bot Missing Permissions",
+                    f"I need the following permissions to execute this command: {perms}"
+                )
+            case commands.CommandOnCooldown():
+                return (
+                    "Command on Cooldown",
+                    f"This command is on cooldown. Try again after {error.retry_after:.2f} seconds."
+                )
+            case commands.NotOwner():
+                return ("Not Owner",
+                        "Only the bot owner can use this command.")
+            case commands.NoPrivateMessage():
+                return (
+                    "No Private Message",
+                    "This command cannot be used in private messages. Please use it in a server channel."
+                )
+            case commands.BadArgument():
+                return ("Bad Argument", str(error))
+            case commands.CheckFailure():
+                return ("Check Failure",
+                        "You do not have permission to execute this command.")
+            case commands.DisabledCommand():
+                return ("Disabled Command",
+                        "This command is currently disabled.")
+            case commands.UserInputError():
+                return ("User Input Error", str(error))
+            case commands.InvalidEndOfQuotedStringError():
+                return ("Invalid End of Quoted String", str(error))
+            case commands.ExpectedClosingQuoteError():
+                return ("Expected Closing Quote", str(error))
+            case commands.MaxConcurrencyReached():
+                return (
+                    "Max Concurrency Reached",
+                    f"This command can only be used {error.number} times concurrently."
+                )
+            case commands.UnexpectedQuoteError():
+                return ("Unexpected Quote", str(error))
+            case discord.Forbidden():
+                return ("Forbidden", "I do not have permission to do that.")
+            case discord.NotFound():
+                return ("Not Found", "The requested resource was not found.")
+            case discord.HTTPException():
+                return ("HTTP Exception", "An HTTP exception occurred.")
+            case _:
+                # Log the error with traceback
+                traceback_str: str = ''.join(
+                    traceback.format_exception(type(error), error,
+                                               error.__traceback__))
+                return ("Unexpected Error", traceback_str)
 
 
 class HelpButton(discord.ui.Button):
 
     def __init__(self, ctx: commands.Context, title: str, description: str,
-                 button_color: discord.ButtonStyle):
+                 button_color: discord.ButtonStyle) -> None:
         super().__init__(label='Help', emoji='❓', style=button_color)
-        self.ctx = ctx
-        self.title = title
-        self.description = description
+        self.ctx: commands.Context = ctx
+        self.title: str = title
+        self.description: str = description
 
-    async def callback(self, interaction: discord.Interaction):
-        help_message = self.get_help_message(self.title)
-        embed = discord.Embed(title=f'Help: {self.title}',
-                              description=help_message,
-                              color=EMBED_COLOR)
+    async def callback(self, interaction: discord.Interaction) -> None:
+        help_message: str = self.get_help_message(self.title)
+        embed: discord.Embed = discord.Embed(title=f'Help: {self.title}',
+                                             description=help_message,
+                                             color=EMBED_COLOR)
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
     def get_help_message(self, error_title: str) -> str:
-        help_messages = {
+        help_messages: dict[str, str] = {
             "Missing Required Argument":
             "You missed a required argument for the command. Please check the command syntax and try again.",
             "Command Not Found":
