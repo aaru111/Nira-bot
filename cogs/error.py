d2b89b97e6a2653bfce290f768a42aa366f6e8eb
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-09-03 03:04:37 +0000

    Refactor code to use match-case statements for cleaner error handling.

--- cogs/error.py
+++ cogs/error.py
@@ -4,45 +4,42 @@ import traceback
 import aiohttp
 import asyncio
 from difflib import get_close_matches
-from typing import Optional, Tuple, Any, List
+from typing import List
 from loguru import logger
 import sys
 import ipdb
 
-
-# Define the embed color
-EMBED_COLOR: int = 0x2f3131
+# Define the base embed color
+DEFAULT_EMBED_COLOR: int = 0x2f3131
 DELETE_AFTER: int = 10  # Time in seconds after which the error message will delete itself
 
-# Configure loguru
+# Configure loguru for logging
 logger.remove()
-logger.add(sys.stderr, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>")
+logger.add(
+    sys.stderr,
+    format=
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+)
 logger.add("bot_errors.log", rotation="500 MB")
 
-# Flag to enable/disable interactive debugging
-DEBUG_MODE = False  # Set this to True to enable interactive debugging
+# Enable/disable interactive debugging
+DEBUG_MODE = False
+
 
 def custom_excepthook(type, value, tb):
-    """Custom exception hook that starts ipdb when an exception occurs."""
+    """Custom exception hook to trigger ipdb for debugging."""
     traceback.print_exception(type, value, tb)
     if DEBUG_MODE:
         ipdb.post_mortem(tb)
 
-# Set the custom exception hook
+
 sys.excepthook = custom_excepthook
 
+
 class Errors(commands.Cog):
-    """
-    A cog to handle and report errors occurring during command execution.
-    """
+    """Cog to handle and report errors during command execution."""
 
     def __init__(self, bot: commands.Bot) -> None:
-        """
-        Initialize the Errors cog.
-
-        Args:
-            bot (commands.Bot): The instance of the bot.
-        """
         self.bot: commands.Bot = bot
         self.session: aiohttp.ClientSession = aiohttp.ClientSession()
 
@@ -53,105 +50,91 @@ class Errors(commands.Cog):
     async def send_error_embed(self, ctx: commands.Context, title: str,
                                description: str,
                                button_color: discord.ButtonStyle) -> None:
-        """
-        Send an embed with error information to the context channel.
-
-        Args:
-            ctx (commands.Context): The context in which the command was invoked.
-            title (str): The title of the embed.
-            description (str): The description of the embed.
-            button_color (discord.ButtonStyle): The color of the help button.
-        """
-        embed: discord.Embed = discord.Embed(title=title,
-                                             description=f"```py\n{description}```",
-                                             color=EMBED_COLOR)
+        """Send an embed with error information to the context channel."""
+        embed_color: int = self.get_embed_color(title)
+        embed: discord.Embed = discord.Embed(
+            title=title,
+            description=f"```py\n{description}```",
+            color=embed_color)
         view: discord.ui.View = discord.ui.View()
         view.add_item(HelpButton(ctx, title, description, button_color))
 
         try:
-            message: discord.Message = await ctx.send(embed=embed, view=view, ephemeral=True)
-            await message.delete(delay=DELETE_AFTER)  # Attempt to delete the message after a delay
+            message: discord.Message = await ctx.send(embed=embed,
+                                                      view=view,
+                                                      ephemeral=True)
+            await message.delete(delay=DELETE_AFTER)
         except discord.errors.NotFound:
-            logger.warning("Failed to send or delete an error embed: message or channel not found.")
+            logger.warning(
+                "Failed to send or delete an error embed: message or channel not found."
+            )
         except discord.errors.Forbidden:
-            logger.warning("Failed to delete the error embed: insufficient permissions.")
+            logger.warning(
+                "Failed to delete the error embed: insufficient permissions.")
 
     async def handle_error(self, ctx: commands.Context,
                            error: commands.CommandError, description: str,
                            title: str) -> None:
-        """
-        Handle sending an error embed based on the error type.
-
-        Args:
-            ctx (commands.Context): The context in which the command was invoked.
-            error (commands.CommandError): The error that was raised.
-            description (str): The description of the error.
-            title (str): The title of the error.
-        """
+        """Handle sending an error embed based on the error type."""
         button_color: discord.ButtonStyle = self.get_button_color(title)
         await self.send_error_embed(ctx, title, description, button_color)
 
-        # Log the error only if it's not a Discord-related error
-        if not isinstance(error, (commands.CommandNotFound, commands.UserInputError, commands.CheckFailure)):
-            logger.error(f"Error in command {ctx.command}: {error}\n{description}")
+        if not isinstance(error,
+                          (commands.CommandNotFound, commands.UserInputError,
+                           commands.CheckFailure)):
+            logger.error(
+                f"Error in command {ctx.command}: {error}\n{description}")
 
-        # If in debug mode, start interactive debugger
         if DEBUG_MODE:
             ipdb.set_trace()
 
     def get_button_color(self, title: str) -> discord.ButtonStyle:
-        """
-        Get the button color based on the seriousness of the error.
-
-        Args:
-            title (str): The title of the error.
-
-        Returns:
-            discord.ButtonStyle: The color of the button.
-        """
-        minor_errors: List[str] = [
-            "Missing Required Argument", "Command Not Found",
-            "User Input Error", "Invalid End of Quoted String",
-            "Expected Closing Quote", "Unexpected Quote", "Bad Argument"
-        ]
-
-        major_errors: List[str] = [
-            "Missing Permissions", "Bot Missing Permissions",
-            "Command on Cooldown", "No Private Message", "Check Failure",
-            "Disabled Command", "NSFW Channel Required"
-        ]
-
-        critical_errors: List[str] = [
-            "Not Owner", "Forbidden", "Not Found", "HTTP Exception",
-            "Max Concurrency Reached", "Unexpected Error"
-        ]
-
+        """Determine button color based on the error severity."""
         match title:
-            case title if title in minor_errors:
+            case "Missing Required Argument" | "Command Not Found" | \
+                 "User Input Error" | "Invalid End of Quoted String" | \
+                 "Expected Closing Quote" | "Unexpected Quote" | "Bad Argument":
                 return discord.ButtonStyle.success  # Green
-            case title if title in major_errors:
+            case "Missing Permissions" | "Bot Missing Permissions" | \
+                 "Command on Cooldown" | "No Private Message" | \
+                 "Check Failure" | "Disabled Command" | "NSFW Channel Required":
                 return discord.ButtonStyle.primary  # Blue
-            case title if title in critical_errors:
+            case "Not Owner" | "Forbidden" | "Not Found" | "HTTP Exception" | \
+                 "Max Concurrency Reached" | "Unexpected Error":
                 return discord.ButtonStyle.danger  # Red
             case _:
                 return discord.ButtonStyle.secondary  # Grey
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
-        """
-        Handle errors that occur during command execution.
+    def get_embed_color(self, title: str) -> int:
+        """Determine embed color based on the error severity."""
+        match title:
+            case "Missing Required Argument" | "Command Not Found" | \
+                 "User Input Error" | "Invalid End of Quoted String" | \
+                 "Expected Closing Quote" | "Unexpected Quote" | "Bad Argument":
+                return 0x57F287  # Green
+            case "Missing Permissions" | "Bot Missing Permissions" | \
+                 "Command on Cooldown" | "No Private Message" | \
+                 "Check Failure" | "Disabled Command" | "NSFW Channel Required":
+                return 0x5865F2  # Blue
+            case "Not Owner" | "Forbidden" | "Not Found" | "HTTP Exception" | \
+                 "Max Concurrency Reached" | "Unexpected Error":
+                return 0xED4245  # Red
+            case _:
+                return DEFAULT_EMBED_COLOR  # Default Grey
 
-        Args:
-            ctx (commands.Context): The context in which the command was invoked.
-            error (commands.CommandError): The error that was raised.
-        """
-        # Handling rate limit errors (HTTP 429)
-        if isinstance(error, discord.errors.HTTPException) and error.status == 429:
-            retry_after: int = int(error.response.headers.get("Retry-After", 5))
-            await ctx.send(f"Rate limit hit! Retrying after {retry_after} seconds...", delete_after=DELETE_AFTER)
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context,
+                               error: commands.CommandError) -> None:
+        """Handle errors that occur during command execution."""
+        if isinstance(error,
+                      discord.errors.HTTPException) and error.status == 429:
+            retry_after: int = int(error.response.headers.get(
+                "Retry-After", 5))
+            await ctx.send(
+                f"Rate limit hit! Retrying after {retry_after} seconds...",
+                delete_after=DELETE_AFTER)
             await asyncio.sleep(retry_after)
 
-            # Retry the command
             try:
                 await ctx.reinvoke()
             except discord.errors.HTTPException as e:
@@ -161,34 +144,21 @@ class Errors(commands.Cog):
                     await self.on_command_error(ctx, e)
             return
 
-        # Handle other errors
         command_name: str = ctx.command.qualified_name if ctx.command else "Unknown Command"
-        command_signature: str = getattr(ctx.command, 'signature', '') if ctx.command else ""
+        command_signature: str = getattr(ctx.command, 'signature',
+                                         '') if ctx.command else ""
 
         title, description = self.get_error_title_and_description(
             ctx, error, command_name, command_signature)
         await self.handle_error(ctx, error, description, title)
 
-        # If in debug mode, start interactive debugger
         if DEBUG_MODE:
             ipdb.set_trace()
 
-    def get_error_title_and_description(self, ctx: commands.Context,
-                                        error: commands.CommandError,
-                                        command_name: str,
-                                        command_signature: str) -> Tuple[str, str]:
-        """
-        Get the title and description for the error embed based on the error type.
-
-        Args:
-            ctx (commands.Context): The context in which the command was invoked.
-            error (commands.CommandError): The error that was raised.
-            command_name (str): The name of the command that triggered the error.
-            command_signature (str): The command signature.
-
-        Returns:
-            tuple: The title and description for the error embed.
-        """
+    def get_error_title_and_description(
+            self, ctx: commands.Context, error: commands.CommandError,
+            command_name: str, command_signature: str) -> tuple[str, str]:
+        """Get the title and description for the error embed based on the error type."""
         match error:
             case commands.MissingRequiredArgument():
                 return (
@@ -196,13 +166,12 @@ class Errors(commands.Cog):
                     f"Argument: '{error.param.name}'\nUsage: {ctx.prefix}{command_name} {command_signature}"
                 )
             case commands.CommandNotFound():
-                attempted_command = ctx.message.content.split()[0][len(ctx.prefix):]
+                attempted_command = ctx.message.content.split(
+                )[0][len(ctx.prefix):]
                 similar_commands: List[str] = get_close_matches(
-                    attempted_command,
-                    [cmd.name for cmd in self.bot.commands],
+                    attempted_command, [cmd.name for cmd in self.bot.commands],
                     n=1,
-                    cutoff=0.6
-                )
+                    cutoff=0.6)
                 if similar_commands:
                     suggestion: str = similar_commands[0]
                     description: str = f"Command not found. Did you mean `{ctx.prefix}{suggestion}`?"
@@ -227,7 +196,8 @@ class Errors(commands.Cog):
                     f"This command is on cooldown. Try again after {error.retry_after:.2f} seconds."
                 )
             case commands.NotOwner():
-                return ("Not Owner", "Only the bot owner can use this command.")
+                return ("Not Owner",
+                        "Only the bot owner can use this command.")
             case commands.NoPrivateMessage():
                 return (
                     "No Private Message",
@@ -236,9 +206,11 @@ class Errors(commands.Cog):
             case commands.BadArgument():
                 return ("Bad Argument", str(error))
             case commands.CheckFailure():
-                return ("Check Failure", "You do not have permission to execute this command.")
+                return ("Check Failure",
+                        "You do not have permission to execute this command.")
             case commands.DisabledCommand():
-                return ("Disabled Command", "This command is currently disabled.")
+                return ("Disabled Command",
+                        "This command is currently disabled.")
             case commands.UserInputError():
                 return ("User Input Error", str(error))
             case commands.InvalidEndOfQuotedStringError():
@@ -259,9 +231,9 @@ class Errors(commands.Cog):
             case discord.HTTPException():
                 return ("HTTP Exception", "An HTTP exception occurred.")
             case _:
-                # Use traceback to get a simplified error message
-                tb_str = traceback.format_exception(type(error), error, error.__traceback__)
-                simplified_tb = ''.join(tb_str[-3:])  # Get the last 3 lines of the traceback
+                tb_str = traceback.format_exception(type(error), error,
+                                                    error.__traceback__)
+                simplified_tb = ''.join(tb_str[-3:])
                 return ("Unexpected Error", simplified_tb)
 
 
@@ -278,7 +250,7 @@ class HelpButton(discord.ui.Button):
         help_message: str = self.get_help_message(self.title)
         embed: discord.Embed = discord.Embed(title=f'Help: {self.title}',
                                              description=help_message,
-                                             color=EMBED_COLOR)
+                                             color=DEFAULT_EMBED_COLOR)
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
     def get_help_message(self, error_title: str) -> str:
@@ -294,7 +266,7 @@ class HelpButton(discord.ui.Button):
             "Command on Cooldown":
             "This command is on cooldown. Please wait a few moments and try again.",
             "Not Owner":
-            "Become the owner of the bot to use owner only commands.\nApply here - https://blogs.mtdv.me/articles/owner-applications",
+            "Only the bot owner can use this command.",
             "No Private Message":
             "This command cannot be used in private messages. Please use it in a server channel.",
             "Bad Argument":
@@ -334,10 +306,5 @@ class HelpButton(discord.ui.Button):
 
 
 async def setup(bot: commands.Bot) -> None:
-    """
-    Load the Errors cog.
-
-    Args:
-        bot (commands.Bot): The instance of the bot.
-    """
+    """Load the Errors cog."""
     await bot.add_cog(Errors(bot))
