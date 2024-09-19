aaa16e56c17a4100899df0f210a9f3e528d6bee2
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-09-19 05:34:52 +0000

    more improved logging

--- cogs/error.py
+++ cogs/error.py
@@ -18,7 +18,9 @@ logger.remove()
 logger.add(
     sys.stderr,
     format=
-    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    backtrace=True,  # Enabling backtrace to catch more detailed error messages
+    diagnose=True    # Captures internal variables for better context
 )
 
 # Enable/disable interactive debugging
