3fd6278d0be5ec1295eba3bb3913d1488f6fe3d5
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-09-24 12:32:58 +0000

    added new message and placeholders for welcomer

--- cogs/welcome.py
+++ cogs/welcome.py
@@ -25,6 +25,12 @@ class WelcomeCmds(commands.Cog):
     @app_commands.checks.has_permissions(manage_guild=True)
     async def set_channel(self, interaction: discord.Interaction,
                           channel: discord.TextChannel):
+        """
+        Set the channel where welcome messages will be sent.
+
+        Parameters:
+        - channel: The text channel to set as the welcome channel.
+        """
         await self.db.execute(
             "INSERT INTO welcome (guild_id, channel_id) VALUES ($1, $2) ON CONFLICT (guild_id) DO UPDATE SET channel_id = $2",
             interaction.guild.id,
@@ -37,6 +43,19 @@ class WelcomeCmds(commands.Cog):
     @app_commands.checks.has_permissions(manage_guild=True)
     async def set_message(self, interaction: discord.Interaction, *,
                           message: str):
+        """
+        Set the custom welcome message for new members.
+
+        Available placeholders:
+        {user.mention} - Mentions the new member
+        {user.name} - The username of the new member
+        {user.id} - The ID of the new member
+        {guild.name} - The name of the server
+        {guild.member_count} - The current member count of the server
+
+        Parameters:
+        - message: The custom welcome message to set.
+        """
         check = await self.db.fetch(
             "SELECT * FROM welcome WHERE guild_id = $1", interaction.guild.id)
         if not check:
@@ -55,6 +74,12 @@ class WelcomeCmds(commands.Cog):
     async def test(self,
                    interaction: discord.Interaction,
                    user: discord.Member = None):
+        """
+        Test the welcome message by simulating a new member join.
+
+        Parameters:
+        - user: The user to simulate as a new member (optional, defaults to the command user).
+        """
         user = user or interaction.user
         await self.on_member_join(user)
         return await interaction.response.send_message(
@@ -66,39 +91,59 @@ class WelcomeCmds(commands.Cog):
             "SELECT * FROM welcome WHERE guild_id = $1", member.guild.id)
         if not record:
             return  # No welcome settings for this guild
-
         channel_id = record[0]['channel_id']
         message = record[0].get('message')
-
         channel = member.guild.get_channel(channel_id)
         if not channel:
             return  # Channel not found
 
         try:
             welcome_card = await get_welcome_card(member)
-        except Exception as e:
-            print(f"Error generating welcome card: {e}")
+        except Exception:
             welcome_card = None
 
-        default_message = f"Welcome to {member.guild.name}, {member.mention}!"
-        send_message = message.format(
-            user=member.mention,
-            guild=member.guild.name) if message else default_message
+        # Prepare placeholder values
+        placeholders = {
+            "user.mention": member.mention,
+            "user.name": member.name,
+            "user.id": member.id,
+            "guild.name": member.guild.name,
+            "guild.member_count": member.guild.member_count,
+        }
+
+        # Default embed message using all placeholders
+        default_embed = discord.Embed(
+            title=f"Welcome to {placeholders['guild.name']}!",
+            description=
+            (f"Hello {placeholders['user.mention']}! Welcome to our community.\n\n"
+             f"You are our {placeholders['guild.member_count']}th member!\n\n"
+             f"Your Details:\n"
+             f"Name: {placeholders['user.name']}\n"
+             f"ID: {placeholders['user.id']}\n\n"
+             "We hope you enjoy your stay!"),
+            color=discord.Color.green())
+        default_embed.set_thumbnail(url=member.display_avatar.url)
+
+        # Format the message with placeholders if a custom message is set
+        if message:
+            embed = discord.Embed(description=message.format(**placeholders),
+                                  color=discord.Color.green())
+            embed.set_thumbnail(url=member.display_avatar.url)
+        else:
+            embed = default_embed
 
         try:
             if welcome_card:
                 await channel.send(
-                    send_message,
+                    embed=embed,
                     file=discord.File(welcome_card, filename="welcome.png"),
                 )
             else:
-                await channel.send(send_message)
+                await channel.send(embed=embed)
         except discord.errors.Forbidden:
-            print(
-                f"Bot doesn't have permission to send messages in {channel.name}"
-            )
-        except Exception as e:
-            print(f"Error sending welcome message: {e}")
+            pass  # Bot doesn't have permission to send messages in the channel
+        except Exception:
+            pass  # Error sending welcome message
 
 
 async def setup(bot: commands.Bot):
