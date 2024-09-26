1f40cc56a8ec161cb85e44e4e1800621c58c450d
aaru111 <85352740+aaru111@users.noreply.github.com>
2024-09-26 05:22:06 +0000

    modularized level.py

--- cogs/automod.py
+++ cogs/automod.py
@@ -1,7 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from typing import Literal, List, Optional, Union
+from typing import Literal, List, Optional
 
 
 class AutoMod(commands.Cog):
